a06fffe20b2d4f587a400c76495f9ffc51a34224
TejaswiR <tejaswi.ramisetty@gmail.com>
2016-05-17 16:05:43 +0530

    final output

--- src/app.js
+++ src/app.js
@@ -12,6 +12,25 @@ $routeProvider
 
 
 
-angularApp.controller("MainController",function(){
+angularApp.controller("MainController",function($resource,$q,$http){
+var vm = this;
 
+// var D = $q.defer();
+// var CricketDetails = $resource("http://cricapi.com/api/cricket");
+
+vm.getData= function(){
+  $http({method : 'GET', url:"http://cricapi.com/api/cricket"})
+    .success(function(result){
+      console.log(result)
+      vm.all_data = result
+      console.log(vm.all_data.data)
+    })
+    .error(function(error){
+      console.log("printing from error")
+      console.log(error)
+    })
+}
+
+  vm.getData();
 });
+
